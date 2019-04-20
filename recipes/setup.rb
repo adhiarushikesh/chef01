ae9217e8b0d8212552bef5bad75f0003584b7181
UB1 <adhiarushikesh@gmail.com>
2019-04-20 05:25:03 +0000

    nesting

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -6,8 +6,19 @@ package 'tree' do
   action :install
 end
 
+# HOSTNAME = node['hostname']
+# IPADDRESS = node['ipaddress']
+# CPU_TOTAL = node['cpu']['0']['mhz']
+# MEM_TOTAL = node['memory']['total']
+
+
 file '/etc/motd' do
-  content 'this system is property of Adhia, please refrain from using'
+  content "this system is property of Adhia, please refrain from using
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']}
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}
+"
   owner 'root'
   group 'root'
   mode '0755'
